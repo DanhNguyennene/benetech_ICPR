17dbba882c868631af0fd3f36ec4a6fc644e969a
DanhNguyennene <danhvm12345@gmail.com>
2024-10-26 21:12:32 +0700

    DEBUG log

--- code/custom_train.py
+++ code/custom_train.py
@@ -441,7 +441,7 @@ def run_train_ddp(rank, world_size, cfg):
             if step % 500 == 0:
                 print_and_log(f"Epoch {epoch + 1}, Step {step + 1}/{len(train_dl)}, "
                               f"Loss: {loss.item():.4f}, Average Loss: {loss_meter.avg:.4f}, "
-                              f"Learning Rate: {scheduler.get_last_lr()[0]:.6f}", logging.INFO)
+                              logging.INFO)
                 # At the end of the epoch, log average metrics
         print_and_log(f"End of epoch {epoch + 1}: Average Loss: {loss_meter.avg}", logging.INFO)
 
